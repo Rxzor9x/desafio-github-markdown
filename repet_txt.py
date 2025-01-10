7aeacd3925965982f85ac34e3796c9191ff35e4c
Vitor Henrique <90453357+VitorAraujo63@users.noreply.github.com>
2025-01-10 20:36:06 +0000

    desafio python no codespace

---
+++ repet_txt.py
@@ -0,0 +1,7 @@
+# Solicitar uma string e um número inteiro como entrada
+texto = input("Digite uma string: ")
+numero = int(input("Digite um número inteiro: "))
+
+# Repetir a string o número de vezes indicado
+
+print(texto * numero)
